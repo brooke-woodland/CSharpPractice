b86789aabefe98e2373f931cbbd7fcab5e901590
brooke-woodland <brooke@woodlandfamily.org>
2024-10-01 13:56:04 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = int.Parse(args[1]);
+        System.Console.WriteLine(args[1] + " squared is " + num * num);
+        System.Console.WriteLine("Hello, " + args[0] +"!");
+    }
+}
